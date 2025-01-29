15963473c4b53315e97766776530c6ca6144fa32
Christian Corona <christian_corona@outlook.com>
2025-01-29 12:21:00 +0100

    feat: Add Posthog analytics integration

--- src/types.ts
+++ src/types.ts
@@ -1,16 +1,29 @@
 export type Site = {
-  NAME: string;
-  EMAIL: string;
-  NUM_WORKS_ON_HOMEPAGE: number;
-  NUM_PROJECTS_ON_HOMEPAGE: number;
-};
+  NAME: string
+  EMAIL: string
+  NUM_WORKS_ON_HOMEPAGE: number
+  NUM_PROJECTS_ON_HOMEPAGE: number
+}
 
 export type Metadata = {
-  TITLE: string;
-  DESCRIPTION: string;
-};
+  TITLE: string
+  DESCRIPTION: string
+}
 
 export type Socials = {
-  NAME: string;
-  HREF: string;
-}[];
+  NAME: string
+  HREF: string
+}[]
+
+type AttributeType = {
+  [key: string]: string
+}
+
+export interface Posthog {
+  init: (arg0: string, arg1: AttributeType) => void
+  capture: (arg0: string, arg1: AttributeType) => void
+  opt_in_capturing: () => void
+  opt_out_capturing: () => void
+  has_opted_out_capturing: () => boolean
+  has_opted_in_capturing: () => boolean
+}
